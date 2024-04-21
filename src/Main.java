32ab5dd73dd2519feb5d0fb84b11b8618656ed62
Steven Nguyen <65553627+tunabearfish@users.noreply.github.com>
2024-04-20 21:31:12 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    String name1 = "Steven";
+    String adjective1 = "Cool";
+    String adjective2 = "Strong";
+    String adjective3 = "Smart";
+    String verb1 = "Running";
+    String noun1 = "dog";
+    String noun2 = "Vape";
+    String noun3 = "Water";
+    String noun4 = "Cookies";
+    String noun5 = "Smoothies";
+    String noun6 = "pie";
+    String name2 = "Kristina";
+    int number = 11;
+    String place1 = "Boston";
+
+
+
+
+
+
+    String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+
+
+
+
+
+
+    System.out.print(story);
+
+
+
+
+
+
+
+    }
+}
